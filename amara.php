46ff8c8747be3f4f3956fdf91bad6f27077ded74
Charles Fulton <mackensen@gmail.com>
2013-03-21 22:46:31 -0400

    Proof of concept

---
+++ amara.php
@@ -0,0 +1,21 @@
+<?php
+/*
+        Plugin Name: Amara
+        Description: Embed-Amara subtitled videos.
+        Version: 0.1
+        Author: Charles Fulton
+        License: GPL2
+*/
+
+function render_amara ( $atts ) {
+        extract( shortcode_atts( array(
+                'url' => ''
+        ), $atts ) );
+
+        if( empty( $url ) ) return '';
+
+        return "<script type=\"text/javascript\" src=\"http://s3.amazonaws.com/s3.www.universalsubtitles.org/embed.js\">({\"video_url\": \"$url\"})</script>";
+
+}
+
+add_shortcode( 'amara', 'render_amara' );
